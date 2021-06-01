d943da965d672c62a92152740a5dd332df41c53d
LeeHanna <71548623+lee1nna@users.noreply.github.com>
2021-06-01 20:40:18 +0900

    data type

---
+++ #2. Datatype/datatype.js
@@ -0,0 +1,22 @@
+// const, let, var
+const a = 221;
+let b = 4;
+var c = 20;
+console.log(a,b,c);
+
+// string
+// const what = hanna;
+const str = "hanna";
+console.log(str);
+
+// Boolean (true:1, false:0)
+const bool = true;
+console.log(bool);
+
+// Number
+const num = 55;
+console.log(num);
+
+// Float
+const flt = 55.1;
+console.log(flt);
