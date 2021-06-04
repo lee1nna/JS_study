5b33dce0648d78efe19619c6910a6f4243ca8cc7
LeeHanna <71548623+lee1nna@users.noreply.github.com>
2021-06-04 15:38:04 +0900

    connect html title

--- #1. Helloworld_JS/index.js
+++ #1. Helloworld_JS/index.js
@@ -1,1 +1,8 @@
-alert("I'm Working. I'm JS.");
+
+// querySelector는 노드의 첫번째 자식을 반환 ( css selector 표기법과 동일 )
+const title = document.querySelector("#title");
+// const title = document.getElementById("title");
+title.innerHTML = "Hi! From JS";
+title.style.color = "red";
+console.dir(document);
+document.title = "I own you now";
