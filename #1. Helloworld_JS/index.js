4668583b5883115d91a451b5afadcbd4a4abeeb4
LeeHanna <71548623+lee1nna@users.noreply.github.com>
2021-06-07 20:01:21 +0900

    click event

--- #1. Helloworld_JS/index.js
+++ #1. Helloworld_JS/index.js
@@ -2,7 +2,10 @@
 // querySelector는 노드의 첫번째 자식을 반환 ( css selector 표기법과 동일 )
 const title = document.querySelector("#title");
 // const title = document.getElementById("title");
-title.innerHTML = "Hi! From JS";
-title.style.color = "red";
-console.dir(document);
-document.title = "I own you now";
+
+function handleClick() {
+    title.style.color = "blue";
+}
+
+// handleResize()라고 쓰지 않는다. 함수를 바로 즉시 호출하지 않기 위해
+title.addEventListener("click", handleClick);
