128c2c060855648752dc7a8d28143e61ecf70626
LeeHanna <71548623+lee1nna@users.noreply.github.com>
2021-06-08 00:03:20 +0900

    toggle event basic

--- #1. Helloworld_JS/index.js
+++ #1. Helloworld_JS/index.js
@@ -1,11 +1,19 @@
-
-// querySelector는 노드의 첫번째 자식을 반환 ( css selector 표기법과 동일 )
 const title = document.querySelector("#title");
-// const title = document.getElementById("title");
+
+const CLICKED_CLASS = "clicked";
 
 function handleClick() {
-    title.style.color = "blue";
+    // const hasClass = title.classList.contains(CLICKED_CLASS);
+    // if(!hasClass) {
+    //     title.classList.add(CLICKED_CLASS);
+    // } else {
+    //     title.classList.remove(CLICKED_CLASS);
+    // }
+    title.classList.toggle(CLICKED_CLASS);
 }
 
-// handleResize()라고 쓰지 않는다. 함수를 바로 즉시 호출하지 않기 위해
-title.addEventListener("click", handleClick);
+function init() {
+    title.addEventListener("click", handleClick);
+}
+
+init();
