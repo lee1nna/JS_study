cbf0814c4a8119380a56b7d4f566e425279a84b7
LeeHanna <71548623+lee1nna@users.noreply.github.com>
2021-10-09 02:10:24 +0900

    닌자 코드

---
+++ Modern_JS/코어 자바스크립트/코드 품질/3.4 닌자 코드.js
@@ -0,0 +1,20 @@
+// 닌자 코드
+
+// 코드 짧게 쓰기
+i = i ? (i < 0 ? Math.max(0, len + i) : i) : 0;
+
+// 이름 재사용하기
+function ninjaFunction(elem) {
+  // 매개변수로 받아온 elem을 이용한 코드
+  elem = clone(elem);
+
+  // elem의 복제(clone)본을 이용한 코드
+}
+
+// 외부 변수 덮어쓰기
+let user = authenticateUser();
+
+function render() {
+  let user = anotherValue();
+}
+
